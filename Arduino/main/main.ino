ebffaff8fef4d77fc21f783c3003ee10cf1bc0c7
Brandon Nay <brandonnay21@gmail.com>
2019-03-21 21:17:39 -0700

    Create main.ino

---
+++ Arduino/main/main.ino
@@ -0,0 +1,22 @@
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM9DS1.h>
+#include <Adafruit_CCS811.h>
+#include <Adafruit_BME680.h>
+#include <bme680.h>
+#include <bme680_defs.h>
+
+#define PRINTER Serial  // Serial for debug - Serial1 for radio
+#define gpsTX   14      // TX3
+#define gpsRX   15      // RX3
+#define radioTX 18      // TX1
+#define radioRX 19      // RX1
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  
+}
