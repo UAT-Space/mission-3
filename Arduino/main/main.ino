5d1f8a7fa84c3888d3103572b7c2f5738ea6e86e
Brandon Nay <branay@uat.edu>
2019-05-10 07:04:23 -0700

    Update main.ino

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -27,7 +27,7 @@
 #define PRESSURE_HPA (1019.3)   // needs to be updated for launch day (sea level hpa)
 
 // interval between data records in millis
-const uint32_t SAMPLE_INTERVAL_MS = 2000;
+const uint32_t SAMPLE_INTERVAL_MS = 3000;
 
 // time in micros for next data record/transmit
 uint32_t logTime;
@@ -79,8 +79,10 @@ void setup() {
   while (!printer);
 
   // start all sensors
-  startComponents();
   pinMode(uvPin, INPUT);
+  pinMode(SDCS, OUTPUT);
+  pinMode(53, OUTPUT);
+  startComponents();
 
   // start on a multiple of the sample interval.
   logTime = micros() / (1000UL * SAMPLE_INTERVAL_MS) + 1;
@@ -159,64 +161,69 @@ void processData() {
 
   // logTime/1M -> seconds elapsed
   printer.print((logTime / 1000000));             printer.print(F(","));
-  file.print((logTime / 1000000));             printer.print(F(","));
+  file.print((logTime / 1000000));             file.print(F(","));
   printer.print(gps.hour);                        printer.print(F(":"));
-  file.print(gps.hour);                        printer.print(F(":"));
+  file.print(gps.hour);                        file.print(F(":"));
   printer.print(gps.minute);                      printer.print(F(":"));
-  file.print(gps.minute);                      printer.print(F(":"));
+  file.print(gps.minute);                      file.print(F(":"));
   printer.print(gps.seconds);                     printer.print(F(","));
-  file.print(gps.seconds);                     printer.print(F(","));
+  file.print(gps.seconds);                     file.print(F(","));
   printer.print(gps.latitudeDegrees, 4);          printer.print(F(","));
-  file.print(gps.latitudeDegrees, 4);          printer.print(F(","));
+  file.print(gps.latitudeDegrees, 4);          file.print(F(","));
   printer.print(gps.longitudeDegrees, 4);         printer.print(F(","));
-  file.print(gps.longitudeDegrees, 4);         printer.print(F(","));
+  file.print(gps.longitudeDegrees, 4);         file.print(F(","));
   printer.print(gps.altitude);                    printer.print(F(","));
-  file.print(gps.altitude);                    printer.print(F(","));
+  file.print(gps.altitude);                    file.print(F(","));
   printer.print(bmpAltitude);                     printer.print(F(","));
-  file.print(bmpAltitude);                     printer.print(F(","));
+  file.print(bmpAltitude);                     file.print(F(","));
   printer.print(bmpEvent.pressure);               printer.print(F(","));
-  file.print(bmpEvent.pressure);               printer.print(F(","));
+  file.print(bmpEvent.pressure);               file.print(F(","));
   printer.print(bmpTemperature);                  printer.print(F(","));
-  file.print(bmpTemperature);                  printer.print(F(","));
+  file.print(bmpTemperature);                  file.print(F(","));
   printer.print(digitalTemp.getTempCByIndex(0));  printer.print(F(","));
-  file.print(digitalTemp.getTempCByIndex(0));  printer.print(F(","));
+  file.print(digitalTemp.getTempCByIndex(0));  file.print(F(","));
   printer.print(ccsTemp);                         printer.print(F(","));
-  file.print(ccsTemp);                         printer.print(F(","));
+  file.print(ccsTemp);                         file.print(F(","));
   printer.print(ccs.geteCO2());                   printer.print(F(","));
-  file.print(ccs.geteCO2());                   printer.print(F(","));
+  file.print(ccs.geteCO2());                   file.print(F(","));
   printer.print(ccs.getTVOC());                   printer.print(F(","));
-  file.print(ccs.getTVOC());                   printer.print(F(","));
+  file.print(ccs.getTVOC());                   file.print(F(","));
   printer.print(uvValue);                         printer.print(F(","));
-  file.print(uvValue);                         printer.print(F(","));
+  file.print(uvValue);                         file.print(F(","));
   printer.print(a.acceleration.x);                printer.print(F(","));
-  file.print(a.acceleration.x);                printer.print(F(","));
+  file.print(a.acceleration.x);                file.print(F(","));
   printer.print(a.acceleration.y);                printer.print(F(","));
-  file.print(a.acceleration.y);                printer.print(F(","));
+  file.print(a.acceleration.y);                file.print(F(","));
   printer.print(a.acceleration.z);                printer.print(F(","));
-  file.print(a.acceleration.z);                printer.print(F(","));
+  file.print(a.acceleration.z);                file.print(F(","));
   printer.print(m.magnetic.x);                    printer.print(F(","));
-  file.print(m.magnetic.x);                    printer.print(F(","));
+  file.print(m.magnetic.x);                    file.print(F(","));
   printer.print(m.magnetic.y);                    printer.print(F(","));
-  file.print(m.magnetic.y);                    printer.print(F(","));
+  file.print(m.magnetic.y);                    file.print(F(","));
   printer.print(m.magnetic.z);                    printer.print(F(","));
-  file.print(m.magnetic.z);                    printer.print(F(","));
+  file.print(m.magnetic.z);                    file.print(F(","));
   printer.print(g.gyro.x);                        printer.print(F(","));
-  file.print(g.gyro.x);                        printer.print(F(","));
+  file.print(g.gyro.x);                        file.print(F(","));
   printer.print(g.gyro.y);                        printer.print(F(","));
-  file.print(g.gyro.y);                        printer.print(F(","));
+  file.print(g.gyro.y);                        file.print(F(","));
   printer.print(g.gyro.z);                        printer.print(F(","));
-  file.print(g.gyro.z);                        printer.print(F(","));
-
-  file.flush();
+  file.print(g.gyro.z);                        file.print(F(","));
   
   // AMG pixel array
   printer.print(F("["));
+  file.print(F("["));
   for (uint8_t i = 0; i < 63; i++) {
     printer.print(pixels[i]);
     printer.print(F(","));
+    file.print(pixels[i]);
+    file.print(F(","));
   }
   printer.print(pixels[63]);
   printer.println(F("]"));
+  file.print(pixels[63]);
+  file.println(F("]"));
+
+  file.flush();
 }
 
 void startAMG() {
@@ -300,7 +307,7 @@ void startSD() {
       error(9);
     }
   }
-  file = SD.open(fileName, "W+");
+  file = SD.open(fileName, FILE_WRITE);
   if (!file) {
     error(24);
   }
@@ -311,11 +318,11 @@ void startTemp() {
 }
 
 void startComponents() {
+  startGPS();
+  startSD();
   startAMG();
   startBMP();
   startCCS();
-  startGPS();
   startLSM();
-  startSD();
   startTemp();
 }
